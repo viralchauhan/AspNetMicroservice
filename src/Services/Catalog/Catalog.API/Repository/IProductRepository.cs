fc17458f5eabecd0ffd49068c6eb75d662847e82
viral.c <viral.chauhan@neosoftmail.com>
2022-10-21 20:49:44 +0530

    impliment Catalog API Developmenet

---
+++ src/Services/Catalog/Catalog.API/Repository/IProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProduct(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        Task<IEnumerable<Product>> GetProductByCategory(string name);
+
+
+        Task CreateProduct(Product product);
+
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+
+
+    }
+}
